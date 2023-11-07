f5dda9910a295f27a73127ae14b43d1b1dc3c18f
porori12 <96765925+porori12@users.noreply.github.com>
2023-11-07 14:58:48 +0900

    [Bronze IV] Title: 영수증, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+X = int(input())
+N = int(input())
+
+total_sum = 0
+
+for i in range(1, N+1):
+    a, b = map(int, input().split())
+    total_sum += a*b
+
+if X == total_sum:
+    print("Yes")
+else:
+    print("No")
