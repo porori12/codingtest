4a6acbbb7c878b334d7ed0a77fbcd9e04fafcf89
porori12 <96765925+porori12@users.noreply.github.com>
2024-02-02 17:22:08 +0900

    [Bronze II] Title: 화성 수학, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for _ in range(T):
+    mars_math = list(map(str, input().split()))
+    answer = 0
+
+    for i in range(len(mars_math)):
+        if i == 0:
+            answer += float(mars_math[i])
+        else:
+            if mars_math[i] == "@":
+                answer *= 3
+            elif mars_math[i] == "%":
+                answer += 5
+            elif mars_math[i] == "#":
+                answer -= 7
+
+    print("%0.2f" % answer)
+
